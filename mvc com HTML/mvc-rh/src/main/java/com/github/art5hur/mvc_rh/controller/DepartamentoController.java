248a56ae0c0d03ee73db5136a407c1bbaf332aee
RenanLeite7 <119589649+RenanLeite7@users.noreply.github.com>
2024-04-26 22:26:39 -0300

    Add files via upload

---
+++ mvc com HTML/mvc-rh/src/main/java/com/github/art5hur/mvc_rh/controller/DepartamentoController.java
@@ -0,0 +1,30 @@
+package com.github.art5hur.mvc_rh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("departamentos")
+public class DepartamentoController {
+
+	@GetMapping("list")
+	public String list(){
+		return "departamento/list";
+	}
+
+	@GetMapping("cargos")
+	public String cargosByDepartamento(){
+		return "departamento/list_cargos";
+}
+
+	@GetMapping("form")
+	public String form(){
+		return "departamento/form";
+	}
+	
+	
+	
+	
+	
+}
---
+++ mvc com HTML/mvc-rh/src/test/java/com/github/art5hur/mvcrh/MvcRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.art5hur.mvcrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
